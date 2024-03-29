b20c1c3cdfa4d087f8a08fac875fc4e4a696ed5e
Nathan Rosenquist <nathanscottrosenquist@gmail.com>
2024-03-29 10:26:46 -0700

    spawn slimes in line of sight, and slightly closer

--- twitch_fragments/outcomes/slime_squad.lua
+++ twitch_fragments/outcomes/slime_squad.lua
@@ -7,8 +7,8 @@ function twitch_slime_squad()
     local additional = 0
     local player = get_player()
     if player == nil then
-        spawn_something("data/entities/animals/giantshooter_weak.xml", Random(90, 120), Random(120, 150), false, false, append_viewer_name)
-        spawn_something("data/entities/animals/giantshooter_weak.xml", Random(90, 120), Random(120, 150), false, false, append_viewer_name)
+        spawn_entity_in_view_random_angle("data/entities/animals/giantshooter_weak.xml", Random(30, 70), Random(70, 130), 20, append_viewer_name)
+        spawn_entity_in_view_random_angle("data/entities/animals/giantshooter_weak.xml", Random(30, 70), Random(70, 130), 20, append_viewer_name)
         return
     end
     local damage_model = EntityGetFirstComponentIncludingDisabled(player, "DamageModelComponent")
@@ -21,23 +21,23 @@ function twitch_slime_squad()
     local toxic_slime_count = Random(2 + additional, 4 + additional)
     local acid_slime_count = Random(1 + additional, 2 + additional)
     local mother_slime_count = Random(1 + additional, 2 + additional)
-    local near_min = 60 + (additional * 10)
-    local near_max = 90 + (additional * 10)
-    local far_min = 150 + (additional * 10)
-    local far_max = 190 + (additional * 10)
+    local near_min = 40 + (additional * 10)
+    local near_max = 70 + (additional * 10)
+    local far_min = 130 + (additional * 10)
+    local far_max = 170 + (additional * 10)
     for i = 1, toxic_slime_count do
         local entity = "data/entities/animals/slimeshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/slimeshooter.xml" end
-        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
+        spawn_entity_in_view_random_angle(entity, Random(near_min, near_max), Random(far_min, far_max), 20, append_viewer_name)
     end
     for i = 1, acid_slime_count do
         local entity = "data/entities/animals/acidshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/acidshooter.xml" end
-        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
+        spawn_entity_in_view_random_angle(entity, Random(near_min, near_max), Random(far_min, far_max), 20, append_viewer_name)
     end
     for i = 1, mother_slime_count do
         local entity = "data/entities/animals/giantshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/giantshooter.xml" end
-        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
+        spawn_entity_in_view_random_angle(entity, Random(near_min, near_max), Random(far_min, far_max), 20, append_viewer_name)
     end
 end

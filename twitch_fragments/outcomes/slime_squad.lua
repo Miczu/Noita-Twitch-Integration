f94288687be73604b6d0d5906ed841a2d21ffb0a
Nathan Rosenquist <nathanscottrosenquist@gmail.com>
2024-03-29 01:43:15 -0700

    naming tweaks

--- twitch_fragments/outcomes/slime_squad.lua
+++ twitch_fragments/outcomes/slime_squad.lua
@@ -15,23 +15,23 @@ function twitch_slime_squad()
     local toxic_slime_count = Random(2 + additional, 4 + additional)
     local acid_slime_count = Random(1 + additional, 2 + additional)
     local mother_slime_count = Random(1 + additional, 2 + additional)
-    local min_x = 60 + (additional * 10)
-    local max_x = 90 + (additional * 10)
-    local min_y = 150 + (additional * 10)
-    local max_y = 190 + (additional * 10)
+    local near_min = 60 + (additional * 10)
+    local near_max = 90 + (additional * 10)
+    local far_min = 150 + (additional * 10)
+    local far_max = 190 + (additional * 10)
     for i = 1, toxic_slime_count do
         local entity = "data/entities/animals/slimeshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/slimeshooter.xml" end
-        spawn_something(entity, Random(min_x, max_x), Random(min_y, max_y), false, false, append_viewer_name)
+        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
     end
     for i = 1, acid_slime_count do
         local entity = "data/entities/animals/acidshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/acidshooter.xml" end
-        spawn_something(entity, Random(min_x, max_x), Random(min_y, max_y), false, false, append_viewer_name)
+        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
     end
     for i = 1, mother_slime_count do
         local entity = "data/entities/animals/giantshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/giantshooter.xml" end
-        spawn_something(entity, Random(min_x, max_x), Random(min_y, max_y), false, false, append_viewer_name)
+        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
     end
 end

5f25583abd04db0b85bf82ae6a674cb587a7adb8
Nathan Rosenquist <carsinbikelanesnyc@gmail.com>
2024-04-08 11:43:13 -0700

    outcomes/slime-squad (improved) (#30)
    * improved spawning and fairness, fixed health check bug
    
    * fairness changes: fewer mobs, raised tier 2 to 110 hp, only some of the mobs spawn in LoS
    
    * updated outcome description with accurate spawn count
    
    ---------
    
    Co-authored-by: Nathan Rosenquist <nathanscottrosenquist@gmail.com>

--- twitch_fragments/outcomes/slime_squad.lua
+++ twitch_fragments/outcomes/slime_squad.lua
@@ -2,42 +2,52 @@
 --A truly toxic ambush
 --enemies
 --150
---Spawns a squad of 4 to 17 enemies consisting of toxic slimes, acid slimes, and mother slimes. Affected by current player health.
+--Spawns a squad of 3 to 13 enemies consisting of toxic slimes, acid slimes, and mother slimes. Affected by current player health.
 function twitch_slime_squad()
     local additional = 0
     local player = get_player()
     if player == nil then
-        spawn_something("data/entities/animals/giantshooter_weak.xml", Random(90, 120), Random(120, 150), false, false, append_viewer_name)
-        spawn_something("data/entities/animals/giantshooter_weak.xml", Random(90, 120), Random(120, 150), false, false, append_viewer_name)
+        spawn_entity_in_view_random_angle("data/entities/animals/giantshooter_weak.xml", 50, 130, 20, append_viewer_name)
+        spawn_entity_in_view_random_angle("data/entities/animals/giantshooter_weak.xml", 50, 130, 20, append_viewer_name)
         return
     end
     local damage_model = EntityGetFirstComponentIncludingDisabled(player, "DamageModelComponent")
     if damage_model ~= nil then
-       local hp = tonumber(ComponentGetValue2(damage_model, "hp"))
+       local hp = tonumber(ComponentGetValue2(damage_model, "hp")) * 25
+       if hp > 110 then additional = additional + 1 end
        if hp > 150 then additional = additional + 1 end
        if hp > 200 then additional = additional + 1 end
-       if hp > 250 then additional = additional + 1 end
     end
-    local toxic_slime_count = Random(2 + additional, 4 + additional)
-    local acid_slime_count = Random(1 + additional, 2 + additional)
-    local mother_slime_count = Random(1 + additional, 2 + additional)
-    local near_min = 60 + (additional * 10)
-    local near_max = 90 + (additional * 10)
-    local far_min = 150 + (additional * 10)
-    local far_max = 190 + (additional * 10)
+    local toxic_slime_count = Random(1 + additional, 2 + additional)
+    local acid_slime_count = Random(1 + additional, 1 + additional)
+    local mother_slime_count = Random(1 + additional, 1 + additional)
+    local min_distance = 125
+    local max_distance = 200
     for i = 1, toxic_slime_count do
         local entity = "data/entities/animals/slimeshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/slimeshooter.xml" end
-        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
+        if i == 1 then
+            spawn_entity_in_view_random_angle(entity, min_distance, max_distance, 30, append_viewer_name)
+        else
+            spawn_something(entity, min_distance, max_distance, false, false, append_viewer_name)
+        end
     end
     for i = 1, acid_slime_count do
         local entity = "data/entities/animals/acidshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/acidshooter.xml" end
-        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
+        if i == 1 then
+            spawn_entity_in_view_random_angle(entity, min_distance, max_distance, 30, append_viewer_name)
+        else
+            spawn_something(entity, min_distance, max_distance, false, false, append_viewer_name)
+        end
     end
     for i = 1, mother_slime_count do
         local entity = "data/entities/animals/giantshooter_weak.xml"
         if Random(1, 100) > 50 + (additional * 12) then entity = "data/entities/animals/giantshooter.xml" end
-        spawn_something(entity, Random(near_min, near_max), Random(far_min, far_max), false, false, append_viewer_name)
+        if i == 1 then
+            spawn_entity_in_view_random_angle(entity, min_distance, max_distance, 30, append_viewer_name)
+        else
+            spawn_something(entity, min_distance, max_distance, false, false, append_viewer_name)
+        end
     end
 end

00d9a90fb4283cd7044f051c1da7725fe4f57960
hexdefined <13652217+hexdefined@users.noreply.github.com>
2024-07-12 23:09:52 +1200

    Dryspell: reduce fire damage and allow smothering

--- files/effects/dryspell_end.lua
+++ files/effects/dryspell_end.lua
@@ -7,4 +7,11 @@ if #players > 0 then
     if stains ~= nil then
         EntitySetComponentIsEnabled(player_id, stains[1], true)
     end
+
+    -- restore fire damage
+    local damage_model = EntityGetComponent(player_id, "DamageModelComponent")
+    if damage_model ~= nil then
+        local fire = ComponentObjectGetValue2(damage_model[1], "damage_multipliers", "fire")
+        ComponentObjectSetValue2(damage_model[1], "damage_multipliers", "fire", fire * 2)
+    end
 end

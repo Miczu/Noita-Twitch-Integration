e4a75630504c797d9c8cf4ef451ede79cfb66752
BMStrom <benjaminmstrom@gmail.com>
2025-05-23 03:53:27 +1200

    Added Anti-Homing shots
    Added new effect that applies the Anti-Homing modifier to the player's projectiles

---
+++ twitch_fragments/outcomes/spiritusigneus_anti_homing_shot.lua
@@ -0,0 +1,33 @@
+--Anti-Homing Shots
+--Makes your projectiles avoid enemies
+--curses
+--100
+--Why are your projectiles afraid of enemies?
+function twitch_spiritusigneus_anti_homing_shot()
+    async(effect_spiritusigneus_anti_homing_shot)
+end
+
+function effect_spiritusigneus_anti_homing_shot()
+    local player
+
+    repeat
+		wait(1);
+		player = get_player_nopoly();
+	until player > 0;
+
+	local found = false
+	local children = EntityGetAllChildren(player)
+	for z=1,#children do
+		if EntityGetName(children[z]) == "spiritusigneus_anti_homing" then
+			local comp = EntityGetFirstComponentIncludingDisabled(children[z],"GameEffectComponent")
+			ComponentSetValue2(comp,"frames",ComponentGetValue2(comp,"frames") + 2100)
+			found=true
+			break
+		end
+	end
+	if found == false then
+		local x,y = EntityGetTransform(player)
+		local c = EntityLoad("mods/twitch-integration/files/entities/misc/effect_anti_homing_shot.xml",x,y)
+		EntityAddChild(player,c)
+	end
+end

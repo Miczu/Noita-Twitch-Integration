d1d58408c65936f907cf2d8498fe9d8494f4bb01
Christian <christian.bankovic@gmail.com>
2025-07-07 15:11:17 -0500

    ok I think this is working

--- files/scripts/steal_wand.lua
+++ files/scripts/steal_wand.lua
@@ -6,29 +6,45 @@ local pos_x, pos_y = EntityGetTransform(victim)
 local inventory = EntityGetFirstComponentIncludingDisabled(victim, "Inventory2Component")
 if inventory ~= nil then
     local active_item = ComponentGetValue2(inventory, "mActualActiveItem")
-    if active_item ~= nil then
-        if EntityHasTag(active_item, "wand") or EntityHasTag(active_item, "item_pickup") then
+    if active_item ~= nil and active_item ~= 0 then
+        -- if EntityHasTag(active_item, "wand") or EntityHasTag(active_item, "item_pickup") or EntityHasTag(active_item, "tablet") then  -- Keeping this if statement here in case we wanna revert anything
+            ComponentSetValue2(inventory, "mForceRefresh", true)
             EntityRemoveFromParent(active_item)
             EntitySetComponentsWithTagEnabled(active_item,"enabled_in_hand",false)
-            EntitySetComponentsWithTagEnabled(active_item,"enabled_in_world",true)
-            --EntityKill(active_item)
+            EntitySetComponentsWithTagEnabled(active_item,"enabled_in_world",true) -- spawn item/wand
+            -- EntityKill(active_item)
             ComponentSetValue2(inventory, "mActualActiveItem", 0)
             ComponentSetValue2(inventory, "mActiveItem", 0)
 
             if EntityHasTag(active_item, "wand") then -- spawn wand ghost
                 EntityLoad("mods/Twitch-integration/files/entities/animals/wand_ghost.xml", pos_x, pos_y)
             end
-            EntitySetTransform(active_item, pos_x, pos_y) -- spawn item/wand
-            if EntityHasTag(active_item, "item_pickup") then -- throw item
+            EntitySetTransform(active_item, pos_x, pos_y)
+            if not EntityHasTag(active_item, "wand") then -- throw item
                 local velcomp_victim = EntityGetFirstComponentIncludingDisabled(victim, "VelocityComponent")
                 if velcomp_victim ~= nil then
                     local vel_x, vel_y = ComponentGetValue2(velcomp_victim, "mVelocity")
                     PhysicsApplyForce(active_item, vel_x*50, vel_y*25-125)
                     PhysicsApplyTorque(active_item, 0.5+vel_x*5)
                 end
+                -- TODO: Fix the code breaking the kick function of items
+
+                -- activate thrown item functionality
+                for _,comp in ipairs(EntityGetComponentIncludingDisabled(active_item, "LuaComponent") or {}) do
+                    local script_throw = ComponentGetValue2(comp, "script_throw_item")
+                    if script_throw and script_throw ~= "" then
+                        print("manually throwing!")
+                        local GetUpdatedEntityID_old = GetUpdatedEntityID
+                        GetUpdatedEntityID = function() return active_item end
+                        dofile_once(script_throw)
+                        throw_item(pos_x, pos_y, pos_x, pos_y-50)
+                        GetUpdatedEntityID = GetUpdatedEntityID_old
+                        break
+                    end
+                end
             end
             --EntityAddChild(wand, active_item)
-        end
+        -- end
     end
 end
 

4a534587e8fe0f21461163db75d7c17bc1179aa2
IndoorKat <indoorcat0@gmail.com>
2025-08-29 17:20:30 +1000

    Outcome: upgraded enemies

---
+++ twitch_fragments/outcomes/upgrade_enemies.lua
@@ -0,0 +1,28 @@
+function twitch_upgrade_enemies()
+    async(effect_upgrade_enemies)
+end
+
+function effect_upgrade_enemies()
+    
+    local player
+
+    repeat
+		wait(1);
+		player = get_player_nopoly();
+	until player > 0;
+
+    if (player) then
+        local x, y = get_player_pos()
+        local thingy = EntityLoad("mods/twitch-integration/files/effects/upgrade_enemies.xml", x, y)
+        EntityAddComponent2( thingy, "UIIconComponent",
+            {
+                name = "Upgraded Enemies",
+                description = "Enemies feel a bit stronger...",
+                icon_sprite_file = "mods/twitch-integration/files/effects/status_icons/enemy_upgrade.png",
+                display_above_head = false,
+                display_in_hud = true,
+                is_perk = false
+            })
+        EntityAddChild(player, thingy)
+    end
+end

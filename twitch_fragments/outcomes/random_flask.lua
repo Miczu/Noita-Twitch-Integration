64cc128ae2373284eb3a0aca4d8c0cf0a860b297
IndoorKat <indoorcat0@gmail.com>
2025-11-06 13:04:44 +1100

    reduce flask speed

--- twitch_fragments/outcomes/random_flask.lua
+++ twitch_fragments/outcomes/random_flask.lua
@@ -20,15 +20,15 @@ function twitch_random_flask()
     local sx,sy = nil,nil
     -- pick a random angle and increase it until we find a valid spawn
     local startAngle = Random() * math.pi * 2
-    local angleStep = math.pi/180
-    local radiusAttempts = 5
+    local angleAttempts = 360
+    local radiusAttempts = 10
     local found = false
-    for i = 0, 360 do
-        local angle = startAngle + i*angleStep
+    for i = 0, angleAttempts do
+        local angle = Random(0,360)*math.pi/180
         for j=1,radiusAttempts do
-            local radius =Random(80,200)
+            local radius =Random(80,160)
             local tempSx,tempSy = x +radius*math.cos(angle), y +radius*math.sin(angle)
-            if not thiccRaytrace(tempSx,tempSy,x,y,5) then
+            if not thiccRaytrace(tempSx,tempSy,x,y-2,10) then
                 sx,sy = tempSx, tempSy
                 found = true
                 break
@@ -53,10 +53,10 @@ function twitch_random_flask()
         local dist = math.sqrt(dx*dx + dy*dy)
         dx = dx/dist
         dy = dy/dist
-        dy = dy - dist/2000 -- try to compensate for gravity
+        dy = dy - dist/200 -- try to compensate for gravity
         local body = EntityGetFirstComponent(entity, "PhysicsBodyComponent")
         local pixelCount = ComponentGetValue2(body, "mPixelCount")
-        local force = 10 * pixelCount
+        local force = 5 * pixelCount
         PhysicsApplyForce(entity, dx * force, dy * force)
         PhysicsApplyTorque(entity, pixelCount * 0.7)
     end

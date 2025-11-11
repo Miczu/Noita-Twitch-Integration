ce5b2ad21fd04e6fbd7ca5ca3183f6f07966e2d9
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2025-11-11 16:53:36 +1030

    The Immortal Snail
    New vote option: Immortal Snail
    Very rare vote, adds the immortal snail to the game permanently when voted in.
    If the vote occurs multiple times in the same runs, multiple snails can be present.
    The snail will steal perks and items if it manages to touch them
    
    README: I was able to test this in a separate mod and have confirmed it worked there but still can't get TI to load properly to confirm it works with the actual mod.
    It should work without issues, but please double check this event functions properly before merging.
    
    Unnecessary snail lore:
    For those unfamiliar with the immortal snail, the immortal snail originates from a thought experiment asking what you would do if you were to receive a large amount of money, but in exchange an immortal snail aware of your location pursued you until the ends of the earth and if it touches you at any time, you would immediately die.
    
    Some variations of the thought experiment involve the user being immortal or other tweaks, but the important part is this concept was added to a mod called FairMod.
    
    In Fairmod the immortal snail manifests as something which will pursue the player indefinitely, will always be aware of their location and will immediately kill the player upon making contact with them.
    
    The snail will keep up with the player by teleporting to a location nearby them if they stray too far away and in true fairmod spirit, the snail will steal any peaks or items it makes contact with to use them against the player. (This wasn't originally an intended feature but ended up making its way in and it was way too funny to leave out lmao)
    
    If you still aren't sure what the immortal snail is, just watch Dunks fairmod videos and you'll get the idea.
    Just like fairmod, this PR description is unnecessarily convoluted.

---
+++ files/scripts/snail_scripts/snail_annihilation.lua
@@ -0,0 +1,65 @@
+local entity_id = GetUpdatedEntityID()
+local x, y = EntityGetTransform(entity_id)
+
+local kill_radius = 5
+local status_clear_radius = 50
+
+local snail_timeout = tonumber(GlobalsGetValue("SnailTimeout", "0"))
+
+if snail_timeout > 0 then
+	local snail_timeout_frame = tonumber(GlobalsGetValue("SnailTimeoutFrame", "0"))
+	if GameGetFrameNum() > snail_timeout_frame then
+		snail_timeout = snail_timeout - 1
+		GlobalsSetValue("SnailTimeout", tostring(snail_timeout))
+		GlobalsSetValue("SnailTimeoutFrame", tostring(GameGetFrameNum()))
+	end
+	return
+end
+
+local nearby_players = EntityGetInRadiusWithTag(x, y, kill_radius, "player_unit") or {}
+
+if #nearby_players <= 0 then 
+	nearby_players = EntityGetInRadiusWithTag(x, y, kill_radius, "polymorphed_player") or {} 
+end
+
+local clearable_players = EntityGetInRadiusWithTag(x, y, status_clear_radius, "player_unit") or {}
+
+if #clearable_players <= 0 then 
+	clearable_players = EntityGetInRadiusWithTag(x, y, status_clear_radius, "polymorphed_player") or {} 
+end
+
+for i, player in ipairs(clearable_players) do
+	EntityRemoveStainStatusEffect(player, "PROTECTION_ALL", 1)
+	local children = EntityGetAllChildren(player) or {}
+	for _, child in ipairs(children) do
+		local game_effect_comp = EntityGetFirstComponent(child, "GameEffectComponent")
+		if game_effect_comp and ComponentGetValue2(game_effect_comp, "effect") == "PROTECTION_ALL" then
+			EntityKill(child)
+		end
+	end
+end
+
+for i, player in ipairs(nearby_players) do
+
+	local hp = 100 / 25
+	local damage_model_comp = EntityGetFirstComponentIncludingDisabled(player, "DamageModelComponent")
+	if damage_model_comp then
+		ComponentSetValue2(damage_model_comp, "wait_for_kill_flag_on_death", false)
+		ComponentSetValue2(damage_model_comp, "invincibility_frames", 0)
+		hp = ComponentGetValue2(damage_model_comp, "hp")
+	end
+
+	EntityInflictDamage(player, hp * 100, "DAMAGE_CURSE", "immortal_snail", "BLOOD_EXPLOSION", 0, 0, entity_id)
+
+	local px, py = EntityGetTransform(player)
+
+	SetRandomSeed(px, py)
+
+	local count = Random(3, 5)
+	-- spawn guts
+	for i = 1, count do
+		EntityLoad("mods/twitch-integration/files/entities/particles/guts/guts" .. Random(1, 5) .. ".xml", px, py)
+	end
+
+	GlobalsSetValue("SnailTimeout", tostring(120))
+end
